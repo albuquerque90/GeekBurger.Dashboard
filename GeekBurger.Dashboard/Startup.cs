8d908b066e9ef98ae590e0cb3af22234a7d5c643
Fernando Albuquerque <fer.albuquerque90@gmail.com>
2018-03-12 21:12:54 -0300

    mudanças startup.cs

--- GeekBurger.Dashboard/Startup.cs
+++ GeekBurger.Dashboard/Startup.cs
@@ -21,7 +21,9 @@ namespace GeekBurger.Dashboard
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services
+                .AddCors()
+                .AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
